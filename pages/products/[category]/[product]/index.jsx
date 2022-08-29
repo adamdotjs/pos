fe054b12fd8c85fe9f9ded804df04f0e2d13734e
avidworks <avidworks@live.com>
2022-08-29 11:27:33 -0400

    add swr, react-hook-form

--- pages/products/[category]/[product]/index.jsx
+++ pages/products/[category]/[product]/index.jsx
@@ -1,13 +1,25 @@
 import { useRouter } from "next/router";
+import useSWR from "swr";
+import { fetcher } from "../../../../lib/fetcher";
 
 const ProductPage = () => {
 	const router = useRouter();
 	const { category, product } = router.query;
+	const { data, error } = useSWR(`/api/product?estimate=${product}`, fetcher);
+	console.log(data);
+
+	if (!data) return <div>Loading...</div>;
+	if (error) return <div>An error occurred.</div>;
 
 	return (
-		<h1>
-			{category} - {product}
-		</h1>
+		<>
+			<h1>
+				{category} - {product}
+			</h1>
+			<form>
+				<p>{JSON.stringify(data)}</p>
+			</form>
+		</>
 	);
 };
 
--- pages/products/[category]/index.jsx
+++ pages/products/[category]/index.jsx
@@ -1,71 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
+import { fetcher } from "../../../lib/fetcher";
 
 const ProductsPage = () => {
 	const router = useRouter();
 	const { category } = router.query;
-	const [products, setProducts] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+	const { data, error } = useSWR(`/api/products?type=${category}`, fetcher);
 
-	useEffect(() => {
-		setIsLoading(true);
-		fetch(`https://api2.tigerpress.com/api/products?type=${category}`, {
-			method: "GET",
-			headers: {
-				Authorization: `Bearer ${window.localStorage.getItem("token")}`,
-				"Content-Type": "application/json",
-				Accept: "application/json",
-				Origin: window.location.hostname,
-			},
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				setProducts(data);
-				setIsLoading(false);
-			})
-			.catch((error) => {
-				console.error(error);
-				setIsLoading(false);
-			});
-	}, [category]);
-	console.log(products);
+	if (error) return <div>An error occurred.</div>;
+	if (!data) return <div>Loading...</div>;
 
-	if (isLoading) {
-		return <span>Loading...</span>;
-	}
-
-	if (!isLoading) {
-		return (
-			<div>
-				<h1 className="text-3xl font-bold">
-					{category[0].toUpperCase() + category.slice(1).toLowerCase()} Products
-				</h1>
-				<ul className="mt-12">
-					{products.length > 0 &&
-						products.map((product) => (
-							<li key={product.id}>
-								<Link href={`/products/${category}/${product.id}`}>
-									<div className="flex items-center gap-4">
-										<div className="relative aspect-square w-24">
-											<Image
-												unoptimized
-												src={`data:image/jpeg;base64, ${product.picture}`}
-												layout="fill"
-												objectFit="contain"
-												alt={product.product}
-											></Image>
-										</div>
-										<span>{product.product}</span>
+	return (
+		<div>
+			<h1 className="text-3xl font-bold">
+				{category[0].toUpperCase() + category.slice(1).toLowerCase()} Products
+			</h1>
+			<ul className="mt-12">
+				{data.length > 0 &&
+					data.map((product) => (
+						<li key={product.id}>
+							<Link href={`/products/${category}/${product.id}`}>
+								<div className="flex items-center gap-4">
+									<div className="relative aspect-square w-24">
+										<Image
+											unoptimized
+											src={`data:image/jpeg;base64, ${product.picture}`}
+											layout="fill"
+											objectFit="contain"
+											alt={product.product}
+										></Image>
 									</div>
-								</Link>
-							</li>
-						))}
-				</ul>
-			</div>
-		);
-	}
+									<span>{product.product}</span>
+								</div>
+							</Link>
+						</li>
+					))}
+			</ul>
+		</div>
+	);
 };
 
 export default ProductsPage;
